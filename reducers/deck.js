790efa01b15b90cfd1d4d4c18bfd9b1bb8e3f43a
Benjamin Norrington <ben@norrington.net>
2016-02-24 15:58:50 +0000

    added extra player for more excitement

--- reducers/deck.js
+++ reducers/deck.js
@@ -7,9 +7,10 @@ const initialState = {
     turn: 0,
     players: [
       {id: 0, name: "Me", hand:[{id:1, text:'Gutenberg prints Bible'},{id:2, text:'Jesus dies'},{id:3, text:'Invention of the Bicycle'}]},
-      {id: 1, name: "Nick", hand:[{id:4, text:'Shakespear born'},{id:5, text:'Queen Victoria dies'},{id:6, text:'Invention of the Ballpont'}]}
+      {id: 1, name: "Nick", hand:[{id:4, text:'Shakespear born'},{id:5, text:'Queen Victoria dies'},{id:6, text:'Invention of the Ballpont'}]},
+      {id: 2, name: "Stu", hand:[{id:7, text:'Chaucer born'},{id:8, text:'Queen Mary dies'},{id:9, text:'Invention of the Something'}]}
     ],
-    timeline: [{id:7, text:'Domestication of Dogs', year:-15000}]
+    timeline: [{id:10, text:'Domestication of Dogs', year:-15000}]
   }
 }
 
